d13e923e603a7c52783518eab53a0bda93a65082
22bca184 <127925774+22bca184@users.noreply.github.com>
2023-10-01 19:15:03 +0530

    Delete Week6/CSV FILE SOLUTION directory

--- Week6/CSV FILE SOLUTION/PROGRAM 3.py
+++
@@ -1,9 +0,0 @@
- # Read Python.txt file and Print unique word with its count.
- 
-uniquewords=0
-with open("c:\\sqlite3\\python.txt","r") as file:
-        l=file.read()
-        word=set(l.split())
-print("total unique words:",len(word))
-print("Unqiue words are:",word)
-
